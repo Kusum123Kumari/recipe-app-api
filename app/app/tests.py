24b4011fc9aa92a84991fb4bb97f0c339ff43ff4
kusum123kumari <kusumnkumari0102@gmail.com>
2024-08-13 16:13:34 +0530

    lint checker 2

--- app/app/tests.py
+++ app/app/tests.py
@@ -4,9 +4,9 @@ from app import calc
 
 class CalcTests(SimpleTestCase):
     def test_add_numbers(self):
-        res = calc.sum(5,6)
-        self.assertEqual(res,11)
+        res = calc.sum(5, 6)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
-        res = calc.subs(10,15)
-        self.assertEqual(res,5)
+        res = calc.subs(10, 15)
+        self.assertEqual(res, 5)
